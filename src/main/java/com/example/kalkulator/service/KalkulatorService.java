9a89d7c8e480a5c3333b1feb4cd4fa86d56c5405
maks <mamonov.maksim.20003@gmail.com>
2022-11-26 22:32:31 +0500

    Написан калькулятор

--- src/main/java/com/example/kalkulator/service/KalkulatorService.java
+++ src/main/java/com/example/kalkulator/service/KalkulatorService.java
@@ -1,5 +1,6 @@
 package com.example.kalkulator.service;
 
+import com.example.kalkulator.exception.CanNotDividedByZeroException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
 
@@ -7,5 +8,39 @@ import org.springframework.web.context.annotation.SessionScope;
 @SessionScope
 public class KalkulatorService {
 
-    public Integer addition(Integer a, Integer b)
+    public void exception() throws CanNotDividedByZeroException {
+            throw new CanNotDividedByZeroException("Одно значение не было введено");
+
+    }
+
+    public Integer plus(Integer a, Integer b) throws CanNotDividedByZeroException {
+        if (a == null || b == null) {
+            exception();
+        }
+        return a + b;
+    }
+
+    public Integer minus(Integer a, Integer b) throws CanNotDividedByZeroException {
+        if (a == null || b == null) {
+            exception();
+        }
+        return a - b;
+    }
+
+    public Integer multiply(Integer a, Integer b) throws CanNotDividedByZeroException {
+        if (a == null || b == null) {
+            exception();
+        }
+        return a * b;
+    }
+
+    public Integer divide(Integer a, Integer b) throws CanNotDividedByZeroException {
+        if (a == null || b == null) {
+            exception();
+        }
+        if (b == 0) {
+            throw new CanNotDividedByZeroException("на ноль делить нельзя");
+        }
+        return a / b;
+    }
 }
